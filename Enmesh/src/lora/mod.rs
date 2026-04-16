a869ac6a8ecbf4605632f675ce890691756cd618
redengin <sholstei@yahoo.com>
2026-04-16 00:42:28 -0400

    in work

--- Enmesh/src/lora/mod.rs
+++ Enmesh/src/lora/mod.rs
@@ -1,7 +1,7 @@
 
 
 
-
+/// used to configure the LoRa radio
 pub struct LoRaModulationConfig {
     pub frequency_hz: u32,
     pub bandwidth: lora_modulation::Bandwidth,
@@ -9,10 +9,11 @@ pub struct LoRaModulationConfig {
     pub coding_rate: lora_modulation::CodingRate,
 }
 
+/// used to configure the LoRa send/receive
 pub struct LoRaPacketConfig {
+    /// smaller preambles minimize power usage
     pub preamble_length: u16,
-    pub implicit_header: bool,
     pub payload_length: u8,
-    pub crc_on: bool,
+    /// packet will only be recognized by a receiver using the same iq mode
     pub iq_inverted: bool,
 }
