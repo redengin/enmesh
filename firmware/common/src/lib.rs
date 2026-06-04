ae9d2d1a97564c8a0c2f584a5b39bd0b5f27e3dc
redengin <sholstei@yahoo.com>
2026-06-03 23:12:54 -0400

    in work

--- firmware/common/src/lib.rs
+++ firmware/common/src/lib.rs
@@ -41,4 +41,5 @@ pub use ssd1306;
 // BLE Host
 //------------------------------------------------------------------------------
 pub use trouble_host;
-pub use rand_core;
+pub use trouble_host_rand_core;
+// pub use trouble_host_embassy_sync;
