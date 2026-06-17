3bf4f2625e8fd4db03a7d706928d9488671dfc05
redengin <sholstei@yahoo.com>
2026-06-16 23:03:18 -0400

    in work

--- MeshCore/src/cli.rs
+++ MeshCore/src/cli.rs
@@ -3,6 +3,7 @@ use crate::lora::MeshCoreHashSize;
 /// https://docs.meshcore.io/cli_commands/
 ///
 /// commands that can be sent to MeshCore Repeaters, Room Servers and Sensors.
+#[derive(Debug, PartialEq)]
 pub enum CliCommands<'a> {
     /// "reboot" - reboot the device
     Reboot,
@@ -27,7 +28,7 @@ pub enum CliCommands<'a> {
     /// "neighbors" - show last 8 received adverts ({pubkey-prefix}:{timestamp}:{snr*4})
     ShowLastAdverts,
     /// "neighbor.remove <pubkey_prefix>"
-    RemoveNeighbor(&'a [u8]),
+    RemoveNeighbor(&'a str),
     /// "discover.neighbors" - discover zero-hop neighbors
     DiscoverZeroHopNeighbors,
 
@@ -259,7 +260,7 @@ pub enum CliCommands<'a> {
     /// "get bridge.delay" - show the bridge delay
     ShowBridgeDelay,
     /// "set bridge.delay <ms>" - set the bridge delay in ms
-    SetBridgeDelay{delay_ms: u16},
+    SetBridgeDelay { delay_ms: u16 },
     /// "get bridge.source" - show the number of pakcets on the bridge
     ShowBridgeSource,
     /// "set bridge.source <source>" - //TODO wtf is this
@@ -267,15 +268,15 @@ pub enum CliCommands<'a> {
     /// "get bridge.baud" - show the baudrate supported by the bridge
     ShowBridgeBaud,
     /// "set bridge.baud <rate>" - rate: [9600, 19200, 38400, 57600, 115200]
-    SetBridgeBaud{baud: u16},
+    SetBridgeBaud { baud: u16 },
     /// "get bridge.channel" - show the channel for the bridge
     ShowBridgeChannel,
     /// "set bridge.channel <channel>" - channel: [1 .. 14]
-    SetBridgeChannel{channel: u8},
+    SetBridgeChannel { channel: u8 },
     /// "get bridge.secret" - show the ESP-NOW secret
     ShowBridgeSecret,
     /// "set bridge.secret <secret>" - set the ESP-NOW secret
-    SetBridgeSecret{ secret: &'a [u8] },
+    SetBridgeSecret { secret: &'a [u8] },
     /// "get bootloader.ver" - show the NRF52 bootloader version
     ShowBootLoaderVersion,
     /// "get pwrmgt.support" - show the power management support
@@ -287,7 +288,101 @@ pub enum CliCommands<'a> {
     /// "get pwrmgt.bootmv" - show teh boot voltage
     ShowBootVoltage,
 }
+impl<'a> CliCommands<'a> {
+    pub fn from_string(s: &'a str) -> Result<Self, &'a str> {
+        {
+            const COMMAND_STRING: &str = "reboot";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::Reboot)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "poweroff";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::PowerOff)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "shutdown";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::PowerOff)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "clkreboot";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::ResetClockAndReboot)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "clock sync";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::ClockSync)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "clock";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::ShowClock)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "time ";
+            if s.starts_with(COMMAND_STRING) {
+                let epoch_string = &s[COMMAND_STRING.len()..];
+                match epoch_string.parse::<u32>() {
+                    Ok(epoch_time) => return Ok(Self::SetClock(epoch_time)),
+                    Err(_) => return Err("<value> must be an integer epoch time")
+                }
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "advert";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::SendFloodAdvert)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "advert.zerohop";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::SendZeroHopAdvert)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "start ota";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::StartOta)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "erase";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::FactoryReset)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "neighbors";
+            if s.eq(COMMAND_STRING) {
+                return Ok(Self::ShowLastAdverts)
+            }
+        }
+        {
+            const COMMAND_STRING: &str = "neighbors.remove ";
+            if s.starts_with(COMMAND_STRING) {
+                let neighbor_string = &s[COMMAND_STRING.len()..];
+                return Ok(Self::RemoveNeighbor(neighbor_string))
+            }
+        }
 
+
+
+
+
+        Err("unknown command {s}")
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub enum LoopDetection {
     Off,
     Minimal,
@@ -295,8 +390,134 @@ pub enum LoopDetection {
     Strict,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum PermissionLevel {
     Guest,
     ReadOnly,
     Admin,
+}
+
+// TESTING
+//--------------------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn cli_commands() {
+        {
+            const COMMAND_STR: &str = "reboot";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::Reboot, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "poweroff";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::PowerOff, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "shutdown";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::PowerOff, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "clkreboot";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::ResetClockAndReboot, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "clock sync";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::ClockSync, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "clock";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::ShowClock, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const EPOCH_TIME: u32 = 1781660399;
+            const COMMAND_STR: &str = "time 1781660399";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::SetClock(EPOCH_TIME), command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "advert";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::SendFloodAdvert, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "advert.zerohop";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::SendZeroHopAdvert, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "start ota";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::StartOta, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "erase";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::FactoryReset, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const COMMAND_STR: &str = "neighbors";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::ShowLastAdverts, command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+        {
+            const NEIGHBOR_STRING: &str = "03";
+            const COMMAND_STR: &str = "neighbors.remove 03";
+            if let Ok(command) = CliCommands::from_string(COMMAND_STR) {
+                assert_eq!(CliCommands::RemoveNeighbor(NEIGHBOR_STRING), command);
+            } else {
+                panic!("failed to parse '{COMMAND_STR}'");
+            }
+        }
+
+
+
+
+
+
+
+    }
 }
