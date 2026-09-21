de731485880bf6fce4ce5c27dfa671d351dda6f8
redengin <sholstei@yahoo.com>
2026-09-20 22:53:44 -0400

    in work

--- firmware/enmesh/examples/binarycolor_ux.rs
+++ firmware/enmesh/examples/binarycolor_ux.rs
@@ -145,6 +145,13 @@ fn run(
         // ux.update(&mut rgb_screen, &state, &theme);
 
         // TODO handle the HID Events
+        if let Some(_hid_event) = button_monitor.update().await
+        {
+            // handle the event
+            // ux.handle_event(hid_event);
+        }
+        else {
+        }
 
         // sleep for a frame period
         const FPS_HZ: u64 = 10;
